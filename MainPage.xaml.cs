50f1751867b03cde39129ab107f9610ae33fb404
Etec Prof. Horácio Augusto da Silveira <discov126@gmail.com>
2025-09-24 22:25:06 -0300

    Projeto V1

---
+++ MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace RoladorDeDados
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void rolagem_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+}
